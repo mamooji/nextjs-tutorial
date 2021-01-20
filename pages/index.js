b7ced96fa83dc2561f561fa5f1c1f294c94d5e5b
mamooji <mamoojim@hotmail.com>
2021-01-20 14:02:14 -0500

    on part 4 of nav tutorial, will finsh this tutorial be EOD

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -54,7 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +205,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ pages/posts/first-post.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const firstPost = () => {
+  return <h1>First Post</h1>;
+};
+
+export default firstPost;
