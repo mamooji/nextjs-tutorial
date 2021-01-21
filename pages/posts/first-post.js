0789be15849f7f49ffdf0b28e0ec39ef61ecda2e
Muhammad Mamooji <mamoojim@hotmail.com>
2021-01-20 21:14:14 -0500

    thought i would have finsished by tn, but im at the dynamic routing part of the tutorial, will finish tomorrow for sure

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,20 @@
-import React from "react";
-
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 const firstPost = () => {
-  return <h1>First Post</h1>;
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
 };
 
 export default firstPost;
